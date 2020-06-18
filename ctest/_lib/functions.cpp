d85381ab31fce26d1ce59d51a85d05125c2edd6d
Debesh-XPS <debesh.mandal@hotmail.com>
2020-06-18 13:05:16 +0100

    added vectorNorm function

--- ctest/_lib/functions.cpp
+++ ctest/_lib/functions.cpp
@@ -1,17 +1,17 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
 double addNumbers (double a, double b) {
     double sum;
     sum = a+b;
     return sum;
 }
 
-void lennardJonesPotential(double *target, double r[], double eps, double sig) {
-    double r2, r6i, result;
-    int count;
-    count = sizeof(r) / sizeof(r[0]);
-    for (int i=0; i<count; ++i) {
-        r2 = sig * sig * r[i] * r[i];
-        r6i = 1/(r2 * r2 * r2);
-        result = (4 * eps) * (r6i*r6i - r6i);
-        target[i] = result;
-    }
+double vectorNorm (std::vector<double> vect) {
+    double result;
+    result = 0;
+    for (int i=0; i<vect.size(); i++)
+        result += vect[i] * vect[i];
+    result = sqrt(result);
+    return result;
 }
